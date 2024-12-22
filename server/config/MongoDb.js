eefc7409855ebbba12e5daaddb77cdd2f07e58e4
AyyazDev <ayyazfakhar@gmail.com>
2024-12-23 01:29:33 +0500

    DB fix

--- server/config/MongoDb.js
+++ server/config/MongoDb.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   try {
+    console.log("MongoDB URI:", process.env.MONGO_URL);  
+
     const conn = await mongoose.connect(process.env.MONGO_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    console.log("MongoDB URI:", process.env.MONGO_URL);  
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);  
   } catch (error) {
