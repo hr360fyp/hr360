ddad3b4fe269136fc9ab6376bcec082329f386f4
AyyazDev <ayyazfakhar@gmail.com>
2025-01-03 23:08:39 +0500

    DB Fix

--- server/config/MongoDb.js
+++ server/config/MongoDb.js
@@ -2,17 +2,22 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   try {
-    console.log("MongoDB URI:", process.env.MONGO_URL);  
+    console.log("MongoDB URI:", process.env.MONGO_URL);
 
+    mongoose.set('strictQuery', false); // Suppress strictQuery warning
+
+    const startTime = Date.now(); // Start time
     const conn = await mongoose.connect(process.env.MONGO_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
+    const endTime = Date.now(); // End time
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);  
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Connection time: ${(endTime - startTime)} ms`);
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`); 
-    process.exit(1);  
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
   }
 };
 
